d6fd583e4dcb47a2c167ebdddbb09e4bef597b71
Hector Fernandes <hectorhu17@gmail.com>
2022-09-07 10:51:18 -0300

    fix: errors

--- PS/src/Main.java
+++ PS/src/Main.java
@@ -14,11 +14,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner in = new Scanner (new FileReader("nome do arquivo.txt")); //Leitura de arquivos
-        while (in.hasNextLine()){
-            String line = in.nextLine();
-            System.out.println(line);
-        }        
+        // Scanner in = new Scanner (new FileReader("nome do arquivo.txt")); //Leitura de arquivos
+        // while (in.hasNextLine()){
+        //     String line = in.nextLine();
+        //     System.out.println(line);
+        // }        
         HashMap<String,String> variables = new HashMap<String, String>(); //Manipulação das tabelas
         variables.put("teste","testando"); //adiciona na tabela
         
